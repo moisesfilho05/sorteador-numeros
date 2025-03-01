c945a3aea7684e911f6c6d0c74af2660c3c98a97
Moisés <filhomoises1708@gmail.com>
2025-03-01 15:34:18 -0300

    refact: adiciona parseInt na função sortear para conversão de valores

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function sortear() {
-    let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;    
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);    
 };
