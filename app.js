3b6ec0d3d9d91b032a69f5422b4145bd06c62d37
Moisés <filhomoises1708@gmail.com>
2025-03-03 22:46:36 -0300

    feat: implementar loop para gerar múltiplos números aleatórios dentro de um intervalo e armazená-los em um array

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ function sortear() {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    };
+
+    console.log(sorteados);
 };
     
 function obterNumeroAleatorio(min, max) {
