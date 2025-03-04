92c6bfceb139a0da045625ac2aa61704b23fd4de
Moisés <filhomoises1708@gmail.com>
2025-03-04 12:13:59 -0300

    feat: adiciona botao reiniciar

--- app.js
+++ app.js
@@ -36,3 +36,11 @@ function alterarStatusBotao() {
         botao.classList.add("container__botao-desabilitado");
     };
 };
+
+function reiniciar() {
+    document.getElementById("quantidade").value = '';
+    document.getElementById("de").value = '';
+    document.getElementById("ate").value = '';
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+};
